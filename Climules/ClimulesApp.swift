a0f7b0b1628759b3c39b0464c047834258a6fefd
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-10-29 14:02:12 -0400

    Initializing swift

---
+++ Climules/ClimulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimulesApp.swift
+//  Climules
+//
+//  Created by Aidan Kwok on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
